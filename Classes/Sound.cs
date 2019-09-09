719648003372cbb939ff722fe27768705f80edfa
Matthew Gibbs <mgibbs@lucidyne.com>
2019-09-09 12:11:10 -0700

    Sound Implemented, Aesthetics only look slightly crap now

---
+++ Classes/Sound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaker.Classes
+{
+    public class Sound
+    {
+        private String path = "";
+        private String type = "";
+
+        public String Path
+        {
+            get => path;
+            set { path = value; }
+        }
+        public String Type
+        {
+            get => type;
+            set { type = value; }
+        }
+    }
+}
